ea58b82d1f075d96ee7b8ac1908b79969454cb17
ZJAllen <zach.allen92@gmail.com>
2019-02-24 14:24:59 -0500

    Reorganization

--- SlushTMC/Devices/__init__.py
+++ SlushTMC/Devices/__init__.py
@@ -1,9 +1,1 @@
 __author__ = 'mangokid'
-
-from SlushTMC import *
-from SlushTMC.Boards import *
-from SlushTMC.Board import *
-from SlushTMC.Base import *
-from SlushTMC.Motor import *
-from SlushTMC.Temprature import *
-from SlushTMC.ExpansionModules import *
